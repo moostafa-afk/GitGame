ca09c7b2714b49f4db33c323650be6e2e839f99d
moostafa-afk <mustafamohammad80002@gmail.com>
2025-12-18 03:59:04 +0200

    The laser beam almost works just need it to stop damaging itself

--- laser_queue.gd
+++ laser_queue.gd
@@ -10,16 +10,30 @@ var point := Vector2.ZERO
 ## Desired pushback speed.
 @export var knockback_strength: float = 500.0
 
-func _physics_process(delta: float) -> void:
-    #var hurtbox = player.get_node("Hurtbox")
+func _physics_process(_delta: float) -> void:
+    
     if self.is_colliding():
         point = self.get_collision_point(0)
-        var collider = self.get_collider(0)
-        if collider:
-            print(collider)
-            collider.get_node("Hurtbox").take_damage(damage, get_knockback(), self)
-            print("it has collided")
+
+        var closest_collider: Node2D = null
+## Inf is infinity, used here to guarantee smallest distance for the first collider
+        var smallest_distance := INF
+
+        for i in range(get_collision_count()):
+           var collider = get_collider(i)
+           var distance_x = to_local(collider.global_position).x - self.global_position.x
+
+           if distance_x  < smallest_distance:
+            ## Sets smallest distance
+               smallest_distance = distance_x 
+             ## Sets closest collider
+               closest_collider = collider
+               print("Closest collider" ,to_local(closest_collider.position).x)
+    
             
+        closest_collider.get_node("Hurtbox").take_damage(damage, get_knockback(), self)
+        print("Self position", self.position.x)
+  
 func get_knockback() -> Vector2:
     var knockback: Vector2
     if knockback_enabled:
